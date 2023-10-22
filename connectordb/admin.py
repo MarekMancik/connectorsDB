32213810eab2be5020e7cc42617f8d3d7bf180b1
unknown <you@example.com>
2023-10-22 21:04:02 +0200

    dokonceny tabulky, přidány první dva zkušební konektory

--- connectordb/admin.py
+++ connectordb/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.admin.options import InlineModelAdmin
 
-from connectordb.models import Connector
+# from .models import NameCon
+
+from connectordb.models import Connector, ConnectorName, Manufacturer, Using, TypeCon, PinNumber, RowNumber
+
 
 class ConnectorAdmin(admin.ModelAdmin):
     list_display = ("part_number", "customer_number", "connector_name", "using", "manufacturer",)
 
 
+
+
 # Register your models here.
-admin.site.register(Connector)
+admin.site.register(Connector, ConnectorAdmin)
+admin.site.register(Manufacturer)   # pridá se do CONECTORDB jako další položka, tu vyplním údaji a ty se zpřístupní při zadávání connectoru
+admin.site.register(ConnectorName)
+admin.site.register(Using)
+admin.site.register(TypeCon)
+admin.site.register(PinNumber)
+admin.site.register(RowNumber)
+
+
--- connectordb/models.py
+++ connectordb/models.py
@@ -2,44 +2,65 @@ from django.db import models
 from django.contrib import auth
 
 # Create your models here.
-class NameCon(models.Model):
+class ConnectorName(models.Model):
     CONNECTOR_NAME = [
-        ("p", "Plug Housing"),
-        ("s", "Socked Housing"),
+        ("Plug Housing", "Plug Housing"),
+        ("Socked Housing", "Socked Housing"),
     ]
 
-    name_con = models.CharField(max_length=1, choices=CONNECTOR_NAME)
+    connector_name = models.CharField(max_length=50, choices=CONNECTOR_NAME)
+
+    def __str__(self):
+        return f"{self.connector_name}"   #bez metody __str__() se v tabulkách hodnoty nezobrazí jménem
+
 
 class Manufacturer(models.Model):
     manufacturer = models.CharField(max_length=15)
 
+    def __str__(self):
+        return f"{self.manufacturer}"
+
 class Using(models.Model):
     USING = [
-        ("w", "Wire Harness"),
-        ("p", "PCB"),
+        ("Wire Harness", "Wire Harness"),
+        ("PCB", "PCB"),
     ]
 
-    using = models.CharField(max_length=1, choices=USING)
+    using = models.CharField(max_length=20, choices=USING)
+
+    def __str__(self):
+        return f"{self.using}"
+
 
 class PinNumber(models.Model):
     pin_number = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.pin_number}"
+
 class RowNumber(models.Model):
-    row_number = models.IntegerField()
+    row_number = models.IntegerField(choices=((1, "1"), (2, "2"), (3, "3")))
+
+    def __str__(self):
+        return f"{self.row_number}"
 
 class TypeCon(models.Model):
     type_con = models.CharField(max_length=15)
 
+    def __str__(self):
+        return f"{self.type_con}"
+
 class Connector(models.Model):
-    part_number = models.CharField(max_length=10)
+    part_number = models.CharField(primary_key=True, max_length=10)
     customer_number = models.CharField(max_length=15)
-    name_con = models.ForeignKey(NameCon, on_delete=models.CASCADE)
+    connector_name = models.ForeignKey(ConnectorName, on_delete=models.CASCADE)
     using = models.ForeignKey(Using, on_delete=models.CASCADE)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     pin_number = models.ForeignKey(PinNumber, on_delete=models.CASCADE)
     row_number = models.ForeignKey(RowNumber, on_delete=models.CASCADE)
     type_con = models.ForeignKey(TypeCon, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return f"{self.connector_name}, {self.using}, {self.manufacturer}"
 
 
